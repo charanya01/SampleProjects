74dbc695abb24f2563885cbbbce8cb2e40a8e91f
Charanya S <scharanya01@gmail.com>
2022-04-22 10:36:13 +0530

    Basic Navigation Controller

---
+++ NavigationController/NavigationController/PinkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PinkViewController.swift
+//  NavigationController
+//
+//  Created by admin on 22/04/22.
+//
+
+import UIKit
+
+class PinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="Pink screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NavigationController/NavigationController/TealViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TealViewController.swift
+//  NavigationController
+//
+//  Created by admin on 22/04/22.
+//
+
+import UIKit
+
+class TealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="Teal Blue"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
